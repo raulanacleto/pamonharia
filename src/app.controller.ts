2876c46d09646d1e5c2ef8e9556e4a7907929da4
raulanacleto <raul@apipass.com.br>
2023-06-15 22:24:49 -0300

    initial

---
+++ src/app.controller.ts
@@ -0,0 +1,17 @@
+import { Controller, Get } from '@nestjs/common';
+import { AppService } from './app.service';
+
+@Controller('/pamonha')
+export class AppController {
+  constructor(private readonly appService: AppService) {}
+ 
+  @Get('/gratuita')
+  getHello(): string {
+    return this.appService.getHello();
+  }
+
+  @Get('/paga')
+  getPaga(): string {
+    return this.appService.getHello();
+  }
+}
